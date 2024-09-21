b1e6242ab42bf3a47d9a7584ec33207f79dd875c
HOJOONKIM <k296070@snu.ac.kr>
2024-09-21 18:58:21 +0900

    Update profile_llama3-3bit-default.sh

--- scripts/profile_llama3-3bit-default.sh
+++ scripts/profile_llama3-3bit-default.sh
@@ -15,7 +15,7 @@ BIT="3BIT"
 
 RESULT_FILE="$MODEL.$BIT.nsys-rep"
 
-RUN_CMD="python $BASE_DIR/test_inference.py -m ../models/Llama-3.1-8B-Instruct-exl2/ -p \"Once upon a time,\" -t 1"
+RUN_CMD="python $BASE_DIR/test_inference.py -m ../models/Llama-3.1-8B-Instruct-exl2/ -p \"Once upon a time,\" -t 2"
 
 eval "nsys profile \
   --output="$RESULT_DIR/$RESULT_FILE" \
