5906e9b9f4294d090fdb3179d8fd9c5c8c9166bb
HOJOONKIM <k296070@snu.ac.kr>
2024-09-21 14:39:25 +0900

    Update profile_llama3-4bit default.sh

--- scripts/profile_llama3-4bit default.sh
+++ scripts/profile_llama3-4bit default.sh
@@ -11,22 +11,22 @@ mkdir -p "$RESULT_DIR"
 
 # FIXME
 MODEL="LLAMA3"
-BIT="4BIT"
+BIT="3BIT"
 
 RESULT_FILE="$MODEL.$BIT.nsys-rep"
 
-RUN_CMD="python $BASE_DIR/test_inference.py -m ../models/Llama-3.1-8B-Instruct-exl2/ -p "Once upon a time," "
+RUN_CMD="python $BASE_DIR/test_inference.py -m ../models/Llama-3.1-8B-Instruct-exl2/ -p 'Once upon a time,' "
 
 nsys profile \
   --output="$RESULT_DIR/$RESULT_FILE" \
   --force-overwrite=true \
   ${RUN_CMD}
 
-nsys stats \
-  --force-overwrite=true \
-  --force-export=true \
-  --output=. \
-  --format=csv \
-  --timeunit=msec \
-  --report=nvtxkernsum \
-  "$RESULT_DIR/$RESULT_FILE"
+#nsys stats \
+#  --force-overwrite=true \
+#  --force-export=true \
+#  --output=. \
+#  --format=csv \
+#  --timeunit=msec \
+#  --report=nvtxkernsum \
+#  "$RESULT_DIR/$RESULT_FILE"
