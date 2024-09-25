3a36b1bc734136881cea351b15662b6a14bf6b4f
HOJOONKIM <k296070@snu.ac.kr>
2024-09-26 05:33:17 +0900

    added generation script

---
+++ scripts/gen-3.sh
@@ -0,0 +1,4 @@
+python convert.py \
+    -i ../models/llama-3-8b-fp16/ \
+    -o ../models/llama-3-8b-3bpw-exl2-H/ \
+    -b 3.0 
---
+++ scripts/gen-4.sh
@@ -0,0 +1,4 @@
+python convert.py \
+    -i ../models/llama-3-8b-fp16/ \
+    -o ../models/llama-3-8b-4bpw-exl2-H/ \
+    -b 4.0 
